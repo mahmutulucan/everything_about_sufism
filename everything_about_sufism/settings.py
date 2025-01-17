76402e862c51168072bd78b40993cf4dc95dbc9f
Mahmut Ulucan <mahmud4700@gmail.com>
2025-01-17 18:37:59 +0100

    Remove CLOUDINARY_MEDIA_URL-2

--- everything_about_sufism/settings.py
+++ everything_about_sufism/settings.py
@@ -205,11 +205,13 @@ USE_CLOUDINARY = os.getenv('USE_CLOUDINARY', 'False') == 'True'
 if USE_CLOUDINARY:
     # Cloudinary settings for media files
     CLOUDINARY_URL = os.getenv('CLOUDINARY_URL')
+    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
     STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticCloudinaryStorage'
 else:
     # Local media settings for development
     MEDIA_ROOT = BASE_DIR / 'media'
     MEDIA_URL = '/media/'
+    DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
 
 # -----------------------------------------------------------------------------
 # Default Primary Key Field Type. Use BigAutoField for primary keys.
